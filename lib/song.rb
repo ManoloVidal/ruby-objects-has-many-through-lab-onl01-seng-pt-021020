14a9e945a287f9c09ed87999d4abf5461b30a99a
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-12 17:37:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,30 @@
  # def self.all
  #   @@all
  # end 
-  
+  require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @all = []
+
+  def initialize(name="", artist="", genre="")
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist
+    @artist
+  end
+
+end
   
   
   
