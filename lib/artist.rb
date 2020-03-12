ef7742a20264dc427500459dcb4514c2ac56338e
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-12 17:10:39 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist 
+  @@all = [] 
+  
+  attr_accessor :name 
+
+
+
+def initialize(name) 
+  @name = name 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+def songs 
+  Song.all.select {|song| song.artist == self}
+end 
+
+end 
