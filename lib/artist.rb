a5f6a81ec18ce3986b99a5abcfbb02e24c4889ad
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-12 17:40:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,22 +14,22 @@ def self.all
   @@all 
 end 
 
-def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    self.songs.last
-  end
+#def new_song(name, genre)
+  #  song = Song.new(name, self, genre)
+  #  self.songs.last
+ # end
 
-def self.songs 
-  Song.all.select do |song|
-    song.artist == self 
-  end 
-end 
+#def self.songs 
+#  Song.all.select do |song|
+ #   song.artist == self 
+ # end 
+#end 
 
-def genres
-    Song.all.map do |song|
-      song.genre
-    end
-  end
+#def genres
+ #   Song.all.map do |song|
+  #    song.genre
+  #  end
+ # end
 
 
 
